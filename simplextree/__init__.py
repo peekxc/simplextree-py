8b34446ca7bbeed4fe9dc0764f6eee61da185b9b
peekxc <matt.piekenbrock@gmail.com>
2023-08-17 16:04:11 -0400

    Bump version to v0.1.0 for release

--- simplextree/__init__.py
+++ simplextree/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# import os
+# import sys
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 __version__ = '0.1.0'
 
