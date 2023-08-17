8e7a15efdf522c6d8b7d715c10b39e5a31e0ccf6
peekxc <matt.piekenbrock@gmail.com>
2023-08-17 12:15:12 -0400

    meson.build changes to switch windows to C++11

--- simplextree/__init__.py
+++ simplextree/__init__.py
@@ -3,6 +3,5 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from .version import VERSION, VERSION_SHORT
 from .SimplexTree import SimplexTree
 from .UnionFind import UnionFind
--- simplextree/version.py
+++
@@ -1,7 +0,0 @@
-## SemVer: https://semver.org/#is-v123-a-semantic-version
-_MAJOR = "0"
-_MINOR = "1"
-_PATCH = "0"
-_SUFFIX = ""
-VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
-VERSION = "{0}.{1}.{2}{3}".format(_MAJOR, _MINOR, _PATCH, _SUFFIX)
